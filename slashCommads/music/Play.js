0ee54d10431ce259e0f6af86e97fded7f8df2148
Jeison Crespo <jeisoncrespo14@gmail.com>
2021-11-08 18:29:15 -0500

    cambios en play

--- slashCommads/music/Play.js
+++ slashCommads/music/Play.js
@@ -40,11 +40,6 @@ module.exports = {
     const stream = await play.stream(vdRepro[0].url);
 
     const embed = {
-      author: {
-        name: "Veneco music",
-        icon_url:
-          "https://external-preview.redd.it/JCfqdlSv6-SdqnOJ9gPE3mEOm7bBhSdfZep3sgfGg6g.jpg?auto=webp&s=b62e343d794bb75e7ed421640c5badc9021b36b1",
-      },
       title: vdRepro[0].title,
       description: `${vdRepro[0].description}\n[LINK](${vdRepro[0].url})`,
       color: "RED",
